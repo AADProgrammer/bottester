7f5c553060755086553745fc0d840775bcaab1e9
AADProgrammer <70822705+AADProgrammer@users.noreply.github.com>
2025-02-05 17:41:36 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+    define("TIME_SY", time() + (3600 * 3));
+    define("YEAR", date("Y", TIME_SY));
+    define("MONTH", date("m", TIME_SY));
+    define("DAY", date("d", TIME_SY));
+
+    define("DEVELOPER_USERNAME", "AAD_SK");
+    define("DEVELOPER_ID", "723013693");
+
+    define("OWNER_ID", "7460601505");
+    define("OWNER_USERNAME", "Cyber_Zone_Admin");
+    
+    define("SUPPORT_USERNAME", "Cyber_Zone_Admin");
+
+    define("CHANNEL_USERNAME", "modern_channel_2025");
+    define("CHANNEL_ID", "-1002268287172");
+
+    //define("BOT_APIKEY_MODERNBOT", "7991119227:AAGSYkj98EdXzEZo9jD6DlbwvZeSMu4efO4");
+    define("BOT_APIKEY", "7571954602:AAEZhMdJ37J5rQsOi5DfacfQzOG9GIQEzWc");
+    define("BOT_NAME", "CyberZone");
+    define("BOT_FOLDER", "CyberZone/");
+    define("BOT_VERSION", "1.0");
+
+    define("SERVER_URL", "https://bot-fabricator.com/");
+    define("FUNCTIONS_URL", SERVER_URL . BOT_FOLDER . "Functions/");
+
+    define("DOLLAR_ICON", " 💰");
+    define("NEW_LINE", "\n");
+    define("EQUALS", "\n========================\n");
+    define("BACK_MAIN_TEXT", "عودة 🔙");
+    define("BACK_TEXT", "رجوع 🔙");
+
+    define("TELLABOT_APIKEY" , "");
+    define("SMSPOOL_APIKEY", "");
+    define("HOTSIM_APIKEY", "");
+    define("DAISYSMS_APIKEY", "");
+?>
